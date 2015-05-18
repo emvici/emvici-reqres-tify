d0fc828338ee5c225163ed27cd6ce9eb1821ab1b
José Moreira <jmoreira@microdual.com>
2015-05-18 20:28:26 +0100

    enhancement for a new release

--- test/.fake.express.js
+++ test/.fake.express.js
@@ -2,15 +2,31 @@ var connect = require( 'connect' ),
     reqrestify = require( '../' ),
     Router = require( 'emvici-router' );
 
-module.exports = function () {
+module.exports = function ( opts ) {
     var app = connect(),
         router = app._router = Router({
             throw404: false,
         });
 
     app.Router = Router;
-    app.use( reqrestify() );
-    app.use( router );
+    app.router = router;
+    app.reqrestify = reqrestify( opts );
+
+    for( var i in app.reqrestify ) {
+      var method = app.reqrestify[ i ];
+
+      if (
+        typeof method !== 'function' ||
+        [ 'handle' ].indexOf( i ) !== -1
+      ) {
+        continue;
+      }
+
+      app[ i ] = method.bind( app.reqrestify );
+    };
+
+    app.use( app.reqrestify );
+    app.use( app.router );
 
     return app;
 };
